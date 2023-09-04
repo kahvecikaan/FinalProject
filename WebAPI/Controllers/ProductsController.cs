7dc7e0fe983dda49bb41e00aa9ad8866ed3dadb3
Kaan Kahveci <kaankahveci29@outlook.com>
2023-09-04 15:06:52 +0300

    - Add ProductsController with CRUD operations. - Update dependency injection configuration for product services in WebAPI. - Modify northwind.sqlite with new product data. - Update FinalProject solution file.

---
+++ WebAPI/Controllers/ProductsController.cs
@@ -0,0 +1,50 @@
+using Business.Abstract;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ProductsController : ControllerBase
+{
+    private readonly IProductService _productService;
+    
+    public ProductsController(IProductService productService)
+    {
+        _productService = productService;
+    }
+    
+    [HttpGet("getall")]
+    public IActionResult GetAll()
+    {
+        var result = _productService.GetAll();
+        if (result.Success)
+        {
+            return Ok(result);
+        }
+        return BadRequest(result);
+    }
+    
+    [HttpPost("add")]
+    public IActionResult Add(Product product)
+    {
+        var result = _productService.Add(product);
+        if (result.Success)
+        {
+            return Ok(result);
+        }
+        return BadRequest(result);
+    }
+    
+    [HttpGet("getbyid")]
+    public IActionResult GetById(int id)
+    {
+        var result = _productService.GetById(id);
+        if (result.Success)
+        {
+            return Ok(result);
+        }
+        return BadRequest(result);
+    }
+}
---
+++ WebAPI/Program.cs
@@ -0,0 +1,32 @@
+using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Abstract;
+using DataAccess.Concrete.EntityFramework;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddSingleton<IProductService, ProductManager>(); // IoC Container, 1 instance of ProductManager for all requests
+builder.Services.AddSingleton<IProductDal, EfProductDal>(); // IoC Container, 1 instance of EfProductDal for all requests
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
