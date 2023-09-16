e5fdcdab31a05a29919ed1f07dd17e94f4496ffa
Kaan Kahveci <kaankahveci29@outlook.com>
2023-09-16 16:23:54 +0300

    feat(Autofac): integrate Castle Proxy for AOP support
    - Configure Autofac to work with Castle Proxy.
    - Register types with interceptors enabled, ensuring AOP aspects function as intended.
    - Utilize AspectInterceptorSelector for selecting relevant interceptors based on attributes.
    
    These changes enhance the modularity and cross-cutting capabilities of the application by leveraging AOP.

--- Business/DependencyResolvers/Autofac/AutofacBusinessModule.cs
+++ Business/DependencyResolvers/Autofac/AutofacBusinessModule.cs
@@ -1,6 +1,9 @@
 using Autofac;
+using Autofac.Extras.DynamicProxy;
 using Business.Abstract;
 using Business.Concrete;
+using Castle.DynamicProxy;
+using Core.Utilities.Interceptors;
 using DataAccess.Abstract;
 using DataAccess.Concrete.EntityFramework;
 
@@ -12,7 +15,16 @@ public class AutofacBusinessModule : Module
     {
         builder.RegisterType<ProductManager>().As<IProductService>().SingleInstance();
         builder.RegisterType<EfProductDal>().As<IProductDal>().SingleInstance();
+        
         builder.RegisterType<CategoryManager>().As<ICategoryService>().SingleInstance();
         builder.RegisterType<EfCategoryDal>().As<ICategoryDal>().SingleInstance();
+        
+        var assembly = System.Reflection.Assembly.GetExecutingAssembly();
+        
+        builder.RegisterAssemblyTypes(assembly).AsImplementedInterfaces()
+            .EnableInterfaceInterceptors(new ProxyGenerationOptions()
+            {
+                Selector = new AspectInterceptorSelector()
+            }).SingleInstance();
     }
 }
