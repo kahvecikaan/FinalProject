2e94a4a03ff1c1e89e032cc27b96b2dd31cd98ef
Kaan Kahveci <kaankahveci29@outlook.com>
2023-09-16 16:25:39 +0300

    Refactor 'ProductName' property

--- Entities/Concrete/Product.cs
+++ Entities/Concrete/Product.cs
@@ -6,7 +6,7 @@ public class Product:IEntity
 {
     public int ProductId { get; set; }
     public int CategoryId { get; set; }
-    public string? ProductName { get; set; }
+    public string ProductName { get; set; } = string.Empty;
     public short UnitsInStock { get; set; }
     public decimal UnitPrice { get; set; }
 }
