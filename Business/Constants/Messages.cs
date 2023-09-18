2cbb54ec5a961e5df0b6495b28028d81579bf8b8
Kaan Kahveci <kaankahveci29@outlook.com>
2023-09-18 14:11:32 +0300

    feat(Business): add new constant messages product operations

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -6,4 +6,8 @@ public static class Messages
     public const string ProductNameInvalid = "Product name invalid.";
     public const string ProductsListed = "Products listed.";
     public const string MaintenanceTime = "Maintenance time.";
+    public const string ProductCountOfCategoryError = "In a category, there can be at most 10 products.";
+    public const string ProductNameAlreadyExists = "Product name already exists.";
+    public const string ProductUpdated = "Product updated.";
+    public const string CategoryLimitExceeded = "Category limit exceeded.";
 }
