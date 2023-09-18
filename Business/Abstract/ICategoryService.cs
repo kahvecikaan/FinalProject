20235725eef6e6dad6796508bd8dc63b8876b14d
Kaan Kahveci <kaankahveci29@outlook.com>
2023-09-18 14:19:04 +0300

    refactor(Business): update ICategoryService return types to use IDataResult

--- Business/Abstract/ICategoryService.cs
+++ Business/Abstract/ICategoryService.cs
@@ -1,9 +1,11 @@
+using Core.Utilities.Results;
 using Entities.Concrete;
 
 namespace Business.Abstract;
 
 public interface ICategoryService
 {
-    List<Category> GetAll();
-    Category GetById(int categoryId);
+    IDataResult<List<Category>> GetAll();
+    IDataResult<Category> GetById(int categoryId);
+    
 }
