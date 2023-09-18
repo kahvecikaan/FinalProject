0312cbef0e7d5d7ae2ce70d8d5a9a04f7118b6e4
Kaan Kahveci <kaankahveci29@outlook.com>
2023-09-18 14:20:17 +0300

    feat(Business): add new method signature to IProductService

--- Business/Abstract/IProductService.cs
+++ Business/Abstract/IProductService.cs
@@ -12,4 +12,5 @@ public interface IProductService
     IDataResult<List<ProductDetailDto>>  GetProductDetails();
     IResult Add(Product product);
     IDataResult<Product> GetById(int productId);
+    IResult Update(Product product);
 }
