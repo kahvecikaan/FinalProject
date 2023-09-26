fe2a4592d9cb92877d16c28d6493bb5705a24a02
Kaan Kahveci <kaankahveci29@outlook.com>
2023-09-26 16:45:36 +0300

    feat(Core): add extensions to Claim and ClaimsPrincipal

---
+++ Core/Extensions/ClaimPrincipalExtensions.cs
@@ -0,0 +1,17 @@
+using System.Security.Claims;
+
+namespace Core.Extensions;
+
+public static class ClaimPrincipalExtensions
+{
+    public static List<string> Claims(this ClaimsPrincipal claimsPrincipal, string claimType)
+    {
+        var result = claimsPrincipal?.FindAll(claimType)?.Select(x => x.Value).ToList();
+        return result ?? new List<string>();
+    }
+    
+    public static List<string> ClaimRoles(this ClaimsPrincipal claimsPrincipal)
+    {
+        return claimsPrincipal?.Claims(ClaimTypes.Role) ?? new List<string>();
+    }
+}
