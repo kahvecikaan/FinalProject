d0908b21d6b37802b03c48c87531471b829308ab
Kaan Kahveci <kaankahveci29@outlook.com>
2023-09-26 16:58:18 +0300

    feat(Business): add new constant messages

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -1,3 +1,5 @@
+using Core.Entities.Concrete;
+
 namespace Business.Constants;
 
 public static class Messages
@@ -10,4 +12,12 @@ public static class Messages
     public const string ProductNameAlreadyExists = "Product name already exists.";
     public const string ProductUpdated = "Product updated.";
     public const string CategoryLimitExceeded = "Category limit exceeded.";
+    public const string AuthorizationDenied = "Authorization denied.";
+    public const string UserAdded = "User added.";
+    public const string UserRegistered = "User registered.";
+    public const string UserNotFound = "User not found.";
+    public const string PasswordError = "Password error.";
+    public const string SuccessfulLogin = "Successful login.";
+    public const string UserAlreadyExists = "User already exists.";
+    public const string AccessTokenCreated = "Access token created.";
 }
