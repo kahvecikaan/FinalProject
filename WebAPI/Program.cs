15c26f15ec1ff3c41cef6aea98f01ba1ad3b3983
Kaan Kahveci <kaankahveci29@outlook.com>
2023-09-26 17:09:25 +0300

    feat(WebAPI): make adjustments to work with authorization functionality

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,13 +1,38 @@
 using Autofac;
 using Autofac.Extensions.DependencyInjection;
 using Business.DependencyResolvers.Autofac;
+using Core.Utilities.IoC;
+using Core.Utilities.Security.Encryption;
+using Core.Utilities.Security.JWT;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Register other services
 builder.Services.AddControllers();
+
+var tokenOptions = builder.Configuration.GetSection("TokenOptions").Get<TokenOptions>();
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidIssuer = tokenOptions.Issuer,
+            ValidAudience = tokenOptions.Audience,
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = SecurityKeyHelper.CreateSecurityKey(tokenOptions.SecurityKey)
+        };
+    });
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
 
 // Use Autofac as the default DI and configure it
 builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory())
@@ -15,6 +40,10 @@ builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory())
     {
         b.RegisterModule(new AutofacBusinessModule());
     });
+
+builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+
+ServiceTool.Create(builder.Services);
 
 var app = builder.Build();
 
@@ -26,6 +55,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
